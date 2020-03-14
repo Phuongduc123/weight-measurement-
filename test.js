a58917a848bb5e9dc4ffcc69668b634cafcd0e57
Dungngo4520 <dungngo4520@gmail.com>
2020-03-14 23:52:03 +0700

    commit on master

---
+++ test.js
@@ -0,0 +1,8 @@
+//Running test
+
+var connect = require('connect');
+var serveStatic = require('serve-static');
+var port = 8080;
+connect().use(serveStatic(__dirname)).listen(port, function(){
+    console.log(`Server running on ${port}...`);
+});
